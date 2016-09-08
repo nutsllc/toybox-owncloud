7606862cb95fba965b748e39c15268d5645e4f2b
@ontheroad_jp <dev@ontheroad.jp>
2016-09-08 17:26:45 +0900

    [New] support SQLite database

--- 9.0.2-apache/docker-entrypoint.sh
+++ 9.0.2-apache/docker-entrypoint.sh
@@ -15,44 +15,58 @@ fi
 
 sh /entrypoint.sh
 
+
 docroot="/var/www/html"
 chown -R ${user}:${group} ${docroot}
 
-db_state=$(mysqladmin ping -h ${MYSQL_PORT_3306_TCP_ADDR} -u ${MYSQL_ENV_MYSQL_USER}  -p${MYSQL_ENV_MYSQL_PASSWORD}) > /dev/null 2>&1
-while [ "${db_state}" != "mysqld is alive" ]; do
-    echo "try to connect db.." && sleep 3
+if [ ${DATABASE:=sqlite} = "mysql" ]; then
     db_state=$(mysqladmin ping -h ${MYSQL_PORT_3306_TCP_ADDR} -u ${MYSQL_ENV_MYSQL_USER}  -p${MYSQL_ENV_MYSQL_PASSWORD}) > /dev/null 2>&1
-done
+    while [ "${db_state}" != "mysqld is alive" ]; do
+        echo "try to connect db.." && sleep 3
+        db_state=$(mysqladmin ping -h ${MYSQL_PORT_3306_TCP_ADDR} -u ${MYSQL_ENV_MYSQL_USER}  -p${MYSQL_ENV_MYSQL_PASSWORD}) > /dev/null 2>&1
+    done
+fi
 
 if [ -f "${docroot}/config/config.php" ]; then
-    sed -i -e "s/'dbhost' => '.*\..*\..*\..*'/'dbhost' => '${MYSQL_PORT_3306_TCP_ADDR}'/g" ${docroot}/config/config.php
+    if [ ${DATABASE:=sqlite} = "mysql" ]; then
+        sed -i -e "s/'dbhost' => '.*\..*\..*\..*'/'dbhost' => '${MYSQL_PORT_3306_TCP_ADDR}'/g" ${docroot}/config/config.php
+    fi
 else
-    sudo -u www-data php /usr/src/owncloud/occ maintenance:install \
-        --database ${DATABASE} \
-        --database-host ${MYSQL_PORT_3306_TCP_ADDR} \
-        --database-name ${MYSQL_ENV_MYSQL_DATABASE} \
-        --database-user ${MYSQL_ENV_MYSQL_USER} \
-        --database-pass ${MYSQL_ENV_MYSQL_PASSWORD} \
-        --admin-user ${OWNCLOUD_USER} \
-        --admin-pass ${OWNCLOUD_PASSWORD}
 
-    sudo -u www-data php /usr/src/owncloud/occ config:system:set trusted_domains \
-        1 --value ${VIRTUAL_HOST:=localhost}
+    if [ ${DATABASE:=sqlite} = "mysql" ]; then
+        sudo -u www-data php /usr/src/owncloud/occ maintenance:install \
+            --database ${DATABASE} \
+            --database-host ${MYSQL_PORT_3306_TCP_ADDR} \
+            --database-name ${MYSQL_ENV_MYSQL_DATABASE} \
+            --database-user ${MYSQL_ENV_MYSQL_USER} \
+            --database-pass ${MYSQL_ENV_MYSQL_PASSWORD} \
+            --admin-user ${OWNCLOUD_USER} \
+            --admin-pass ${OWNCLOUD_PASSWORD}
 
-    sudo -u www-data php /usr/src/owncloud/occ config:system:set logtimezone \
-        --value=$(date +%Z)
+        sudo -u www-data php /usr/src/owncloud/occ config:system:set trusted_domains \
+            1 --value ${VIRTUAL_HOST:=localhost}
 
-    sudo -u www-data php /usr/src/owncloud/occ config:system:set memcache.local \
-        --value="\OC\Memcache\APCu"
+        sudo -u www-data php /usr/src/owncloud/occ config:system:set logtimezone \
+            --value=$(date +%Z)
 
-    sudo -u www-data php /usr/src/owncloud/occ config:system:set memcache.locking \
-        --value="\OC\Memcache\Redis"
+        ## for APC
+        sudo -u www-data php /usr/src/owncloud/occ config:system:set memcache.local \
+            --value="\OC\Memcache\APC"
 
-    sudo -u www-data php /usr/src/owncloud/occ config:system:set redis \
-        'host' --value redis
+        ## for APCu
+        #sudo -u www-data php /usr/src/owncloud/occ config:system:set memcache.local \
+        #    --value="\OC\Memcache\APCu"
 
-    sudo -u www-data php /usr/src/owncloud/occ config:system:set redis \
-        'port' --value 6379
+        ## for Redis
+        #sudo -u www-data php /usr/src/owncloud/occ config:system:set memcache.locking \
+        #    --value="\OC\Memcache\Redis"
+
+        #sudo -u www-data php /usr/src/owncloud/occ config:system:set redis \
+        #    'host' --value redis
+
+        #sudo -u www-data php /usr/src/owncloud/occ config:system:set redis \
+        #    'port' --value 6379
+    fi
 fi
 
 exec "$@"
